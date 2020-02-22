fa195934daad61b3f5d1bfeb946d172a8e20d36f
DoszhanAlibekov <60898549+DoszhanAlibekov@users.noreply.github.com>
2020-02-22 17:58:36 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,93 @@
+var Countries = [
+  {
+    "name": "Estonia",
+    "longitude": 25.738220,
+    "latitute": 58.772472,
+    "Independence": "20-Aug-91",
+  },
+  {
+    "name": "Latvia",
+    "longitude": 24.853821,
+    "latitute": 56.956957,
+    "Independence": "17-Sep-91",
+  },
+  {
+    "name": "Lithuania",
+    "longitude": 23.941956,
+    "latitute": 55.475740,
+    "Independence": "18-Sep-91",
+  },
+  {
+    "name": "Kazakhstan",
+    "longitude":  67.741699,
+    "latitute": 48.327039,
+    "Independence": "16-Dec-91",
+  },
+  {
+    "name": "Kyrgyzstan",
+    "longitude": 74.328003,
+    "latitute": 41.479776,
+    "Independence": "31-Aug-91",
+  },
+  {
+    "name": "Tajikistan",
+    "longitude": 71.076050,
+    "latitute": 38.861098,
+    "Independence": "9-Sep-91",
+  },
+  {
+    "name": "Turkmenistan",
+    "longitude": 59.397583,
+    "latitute": 39.151363,
+    "Independence": "27-Aug-91",
+  },
+  {
+    "name": "Uzbekistan",
+    "longitude": 62.940674,
+    "latitute": 41.787697,
+    "Independence": "31-Aug-91",
+    "Info": "I'll include some information in here"
+  },
+  {
+    "name": "Belarus",
+    "longitude": 28.075562,
+    "latitute": 53.527248,
+    "Independence": "25-Aug-91",
+  },
+  {
+    "name": "Moldova",
+    "longitude": 28.594666,
+    "latitute": 47.189712,
+    "Independence": "27-Aug-91",
+  },
+  {
+    "name": "Ukraine",
+    "longitude": 31.283569,
+    "latitute": 49.325122,
+    "Independence": "24-Aug-91",
+  },
+  {
+    "name": "Russia",
+    "longitude": 96.394043,
+    "latitute": 61.918271,
+    "Independence": "30-Dec-22",
+  },
+  {
+    "name": "Armenia",
+    "longitude": 44.655304,
+    "latitute": 40.605612,
+    "Independence": "21-Sep-91",
+  },
+  {
+    "name": "Azerbaijan",
+    "longitude": 47.710876,
+    "latitute": 40.413496,
+    "Independence": "30-Aug-91",
+  },
+  {
+    "name": "Georgia",
+    "longitude": 43.525085,
+    "latitute": 42.155259,
+    "Independence": "9-Apr-91",
+  }
+]
